c14c4bb6466ad57ba4aedf3cb0d92f0675f2d821
Jos van Bakel <jos@codeaddict.org>
2016-02-05 09:22:27 +0100

    And here is the missing identity resource :)

---
+++ manifests/identity_resource.pp
@@ -0,0 +1,13 @@
+define users::identity_resource(
+  $resource_name,
+  $user,
+  $home,
+) {
+
+  create_resources($resource_name, 
+
+  create_resources($name, $hash[$name], {
+    user => $user,
+  })
+
+}
