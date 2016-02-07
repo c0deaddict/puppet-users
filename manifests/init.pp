43d6a1b4ca98aab296dca2c33e9c4ed93452b44f
Jos van Bakel <josvanbakel@gmail.com>
2016-02-07 16:51:41 +0100

    Comply with puppet lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,9 +5,11 @@ define users(
 
   include stdlib
 
-  case $match {
-    'all','any': { $_match = 'all' }
-    'first':     { $_match = 'first' }
+  $_match = $match ? {
+    'any'   => 'all',
+    'all'   => 'all',
+    'first' => 'first',
+    default => 'all',
   }
 
   if $hash {
@@ -18,21 +20,24 @@ define users(
     # only
     case $_match {
       'all': {
-      $_hash = hiera_hash("users_${name}", undef)
+        $_hash = hiera_hash("users_${name}", undef)
       }
       'first': {
-      $_hash = hiera("users_${name}", undef)
+        $_hash = hiera("users_${name}", undef)
+      }
+      default {
+        fail('unreachable')
       }
     }
   }
   
   if $_hash {
     $users = keys($_hash)
-    users::setup { 
+    users::setup {
       $users:
-        hash       => $_hash;
+        hash => $_hash;
     }
   } else {
-    notify { "no data for resource '$name' title '$title'": }
+    notify { "no data for resource '${name}' title '${title}'": }
   }
 }
